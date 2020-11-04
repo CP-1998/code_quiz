c88490ee153db35155375cc4b6ff8ad7ad27eaf7
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2020-11-04 00:12:11 -0800

    final upload

--- app.js
+++ app.js
@@ -1,227 +1,173 @@
-let score = ""
+(function () {
+  // Functions
+  function buildQuiz() {
+    // variable to store the HTML output
+    const output = [];
 
-function myQuiz1() {
-  var myStart = document.getElementById('start');
-  myStart.remove();
-  document.getElementById('intro').innerHTML = 'Commonly used data types do NOT include...'
-  let quest1 = document.createElement('button');
-  quest1.setAttribute('id', 'btn1')
-  let quest1node = document.createTextNode('1. Strings');
-  quest1.appendChild(quest1node);
-  let quizBod = document.getElementById('quiz');
-  quizBod.appendChild(quest1);
-  quest1.addEventListener('click', myQuiz2);
-  quest4.onclick = function () {
-    let wrongDiv2 = document.getElementById('rightOrWrong')
-    let wrong2 = document.createElement('p')
-    let wrong2Node = document.createTextNode('C O R R E C T')
-    wrong2.appendChild(wrong2Node)
-    wrongDiv2.appendChild(wrong2)
-    score++
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-    let quest2 = document.createElement('button');
-    quest2.setAttribute('id', 'btn2');
-    let quest2node = document.createTextNode('2. Boolean');
-    quest2.appendChild(quest2node);
-    let btnSec2 = document.getElementById('btnSec2');
-    btnSec2.appendChild(quest2);
-    quest2.addEventListener('click', myQuiz2);
-    quest4.onclick = function () {
-      let wrongDiv3 = document.getElementById('rightOrWrong')
-      let wrong3 = document.createElement('p')
-      let wrong3Node = document.createTextNode('C O R R E C T')
-      wrong3.appendChild(wrong3Node)
-      wrongDiv3.appendChild(wrong3)
-      score++
+        // variable to store the list of possible answers
+        const answers = [];
 
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
 
-      let quest3 = document.createElement('button');
-      quest3.setAttribute('id', 'btn3');
-      let quest3node = document.createTextNode('3. Alerts');
-      quest3.appendChild(quest3node);
-      let btnSec3 = document.getElementById('btnSec3');
-      btnSec3.appendChild(quest3);
-      quest3.addEventListener('click', myQuiz2);
-      quest3.onclick = function () {
-        let wrongDiv1 = document.getElementById('rightOrWrong')
-        let wrong1 = document.createElement('p')
-        let wrong1Node = document.createTextNode('W R O N G')
-        wrong1.appendChild(wrong1Node)
-        wrongDiv1.appendChild(wrong1)
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
       }
+    );
 
-      let quest4 = document.createElement('button');
-      quest4.setAttribute('id', 'btn4');
-      let quest4node = document.createTextNode('4. Arrays');
-      quest4.appendChild(quest4node);
-      let btnSec4 = document.getElementById('btnSec4');
-      btnSec4.appendChild(quest4);
-      quest4.addEventListener('click', myQuiz2);
-      quest4.onclick = function () {
-        let wrongDiv2 = document.getElementById('rightOrWrong')
-        let wrong2 = document.createElement('p')
-        let wrong2Node = document.createTextNode('C O R R E C T')
-        wrong2.appendChild(wrong2Node)
-        wrongDiv2.appendChild(wrong2)
-        score++
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults() {
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
       }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+      previousButton.style.display = 'none';
     }
-
-    function myQuiz2() {
-      document.getElementById('btn1').remove()
-      document.getElementById('btn2').remove()
-      document.getElementById('btn3').remove()
-      document.getElementById('btn4').remove()
-      document.getElementById('intro').innerHTML = 'The condition in an if/else statement is closed in...'
-      let questi1 = document.createElement('button');
-      questi1.setAttribute('id', 'bton1');
-      let questi1node = document.createTextNode('1. Quotes');
-      questi1.appendChild(questi1node);
-      let quizBod = document.getElementById('quiz');
-      quizBod.appendChild(questi1);
-      questi1.addEventListener('click', myQuiz3);
-
-      let questi2 = document.createElement('button');
-      questi2.setAttribute('id', 'bton2');
-      let questi2node = document.createTextNode('2. Curly Brackets');
-      questi2.appendChild(questi2node);
-      let btnSec2 = document.getElementById('btnSec2');
-      btnSec2.appendChild(questi2);
-      questi2.addEventListener('click', myQuiz3);
-
-      let questi3 = document.createElement('button');
-      questi3.setAttribute('id', 'bton3');
-      let questi3node = document.createTextNode('3. Parenthesis');
-      questi3.appendChild(questi3node);
-      let btnSec3 = document.getElementById('btnSec3');
-      btnSec3.appendChild(questi3);
-      questi3.addEventListener('click', myQuiz3);
-
-      let questi4 = document.createElement('button');
-      questi4.setAttribute('id', 'bton4');
-      let questi4node = document.createTextNode('4. Square Brackets');
-      questi4.appendChild(questi4node);
-      let btnSec4 = document.getElementById('btnSec4');
-      btnSec4.appendChild(questi4);
-      questi4.addEventListener('click', myQuiz3);
+    else {
+      previousButton.style.display = 'inline-block';
     }
-
-    function myQuiz3() {
-      document.getElementById('bton1').remove()
-      document.getElementById('bton2').remove()
-      document.getElementById('bton3').remove()
-      document.getElementById('bton4').remove()
-      document.getElementById('intro').innerHTML = 'Arrays in JavaScript can be used to store...'
-      let questio1 = document.createElement('button');
-      questio1.setAttribute('id', 'butn1');
-      let questio1node = document.createTextNode('1. Numbers and Strings');
-      questio1.appendChild(questio1node);
-      let quizBod = document.getElementById('quiz');
-      quizBod.appendChild(questio1);
-      questio1.addEventListener('click', myQuiz4);
-
-      let questio2 = document.createElement('button');
-      questio2.setAttribute('id', 'butn2');
-      let questio2node = document.createTextNode('2. Other Arrays');
-      questio2.appendChild(questio2node);
-      let btnSec2 = document.getElementById('btnSec2');
-      btnSec2.appendChild(questio2);
-      questio2.addEventListener('click', myQuiz4);
-
-      let questio3 = document.createElement('button');
-      questio3.setAttribute('id', 'butn3');
-      let questio3node = document.createTextNode('3. Booleans');
-      questio3.appendChild(questio3node);
-      let btnSec3 = document.getElementById('btnSec3');
-      btnSec3.appendChild(questio3);
-      questio3.addEventListener('click', myQuiz4);
-
-      let questio4 = document.createElement('button');
-      questio4.setAttribute('id', 'butn4');
-      let questio4node = document.createTextNode('4. All of the above');
-      questio4.appendChild(questio4node);
-      let btnSec4 = document.getElementById('btnSec4');
-      btnSec4.appendChild(questio4);
-      questio4.addEventListener('click', myQuiz4);
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
     }
-
-    function myQuiz4() {
-      document.getElementById('butn1').remove()
-      document.getElementById('butn2').remove()
-      document.getElementById('butn3').remove()
-      document.getElementById('butn4').remove()
-      document.getElementById('intro').innerHTML = 'String values must be closed within _____ when being assigned to variables...'
-      let question1 = document.createElement('button');
-      question1.setAttribute('id', 'bttn1');
-      let question1node = document.createTextNode('1. Commas');
-      question1.appendChild(question1node);
-      let quizBod = document.getElementById('quiz');
-      quizBod.appendChild(question1);
-      question1.addEventListener('click', myQuiz5);
-
-      let question2 = document.createElement('button');
-      question2.setAttribute('id', 'bttn2');
-      let question2node = document.createTextNode('2. Curly Brackets');
-      question2.appendChild(question2node);
-      let btnSec2 = document.getElementById('btnSec2');
-      btnSec2.appendChild(question2);
-      question2.addEventListener('click', myQuiz5);
-
-      let question3 = document.createElement('button');
-      question3.setAttribute('id', 'bttn3');
-      let question3node = document.createTextNode('3. Quotes');
-      question3.appendChild(question3node);
-      let btnSec3 = document.getElementById('btnSec3');
-      btnSec3.appendChild(question3);
-      question3.addEventListener('click', myQuiz5);
-
-      let question4 = document.createElement('button');
-      question4.setAttribute('id', 'bttn4');
-      let question4node = document.createTextNode('4. Parenthesis');
-      question4.appendChild(question4node);
-      let btnSec4 = document.getElementById('btnSec4');
-      btnSec4.appendChild(question4);
-      question4.addEventListener('click', myQuiz5);
+    else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
     }
+  }
 
-    function myQuiz5() {
-      document.getElementById('bttn1').remove()
-      document.getElementById('bttn2').remove()
-      document.getElementById('bttn3').remove()
-      document.getElementById('bttn4').remove()
-      document.getElementById('intro').innerHTML = 'A very useful tool during development and debugging for printing content to the debugger is...'
-      let questione1 = document.createElement('button');
-      let questione1node = document.createTextNode('1. JavaScript');
-      questione1.appendChild(questione1node);
-      let quizBod = document.getElementById('quiz');
-      quizBod.appendChild(questione1);
-      questione1.addEventListener('click', fin);
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
 
-      let questione2 = document.createElement('button');
-      let questione2node = document.createTextNode('2. Terminal');
-      questione2.appendChild(questione2node);
-      let btnSec2 = document.getElementById('btnSec2');
-      btnSec2.appendChild(questione2);
-      questione2.addEventListener('click', fin);
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
 
-      let questione3 = document.createElement('button');
-      let questione3node = document.createTextNode('3. For Loops');
-      questione3.appendChild(questione3node);
-      let btnSec3 = document.getElementById('btnSec3');
-      btnSec3.appendChild(questione3);
-      questione3.addEventListener('click', fin);
-
-      let questione4 = document.createElement('button');
-      let questione4node = document.createTextNode('4. Console.log');
-      questione4.appendChild(questione4node);
-      let btnSec4 = document.getElementById('btnSec4');
-      btnSec4.appendChild(questione4);
-      questione4.addEventListener('click', fin);
+  // Variables
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "Commonly used data types do NOT include...",
+      answers: {
+        a: "Integers",
+        b: "Strings",
+        c: "Alerts"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "String values must be closed in _____ When being assigned to variables",
+      answers: {
+        a: "Commas",
+        b: "Parenthesis",
+        c: "Quotations"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "A very useful tool during development and debugging for printing content to the debugger is...",
+      answers: {
+        a: "Terminal",
+        b: "Javascript",
+        c: "console.log()"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
     }
+  ];
 
-    function fin() {
-      let finalScore = createElement('p');
-      let finalScoreNode = document.createTextNode(`Your Score Was:`);
-    }
+  // Kick things off
+  buildQuiz();
 
-    document.getElementById('start').addEventListener("click", myQuiz1)
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show the first slide
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
